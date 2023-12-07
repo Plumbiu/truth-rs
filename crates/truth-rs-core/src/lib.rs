ab1c9585c36dee4127035ed422e15381d0196e0c
Plumbiu <3434909403@qq.com>
2023-12-07 11:29:29 +0800

    perf: read flat node_modules floder

--- crates/truth-rs-core/src/lib.rs
+++ crates/truth-rs-core/src/lib.rs
@@ -11,47 +11,60 @@ use std::{
 use truth_rs_type::{package::Package, Relation, RelationsMap};
 use util::merge_map;
 
-pub fn do_gen_relations(relations: &mut RelationsMap, p: &Path) {
+fn insert_relations_one(relations: &mut RelationsMap, dir: &PathBuf, name: Option<String>) {
+    let package_json_path = Path::join(dir, "package.json");
+    let pkg = fs::read(package_json_path).unwrap();
+    let pkg: Package = serde_json::from_slice(&pkg).unwrap();
+    relations.insert(
+        match name {
+            Some(t) => t,
+            None => pkg.name.to_owned(),
+        },
+        Relation {
+            name: pkg.name,
+            version: pkg.version,
+            packages: merge_map(pkg.dependencies, pkg.devDependencies),
+        },
+    );
+}
+
+fn insert_relations_many(relations: &mut RelationsMap, dir: &PathBuf, name: Option<String>) {
+    let dirs = fs::read_dir(&dir).unwrap();
+    for sub_dir in dirs {
+        insert_relations_one(relations, &sub_dir.unwrap().path(), name.clone());
+    }
+}
+
+fn insert_relations(relations: &mut RelationsMap, p: &Path) {
     let dirs = p.read_dir().unwrap();
     for dir in dirs {
-        let entry = dir.unwrap();
-        let path = entry.path();
-        let pkg_path = path.join("package.json");
-        if pkg_path.exists() {
-            let pkg = fs::read_to_string(pkg_path).unwrap();
-            let pkg: Package = serde_json::from_str(&pkg).unwrap();
-            relations.insert(
-                pkg.name.to_owned(),
-                Relation {
-                    name: pkg.name,
-                    version: pkg.version,
-                    packages: merge_map(pkg.dependencies, pkg.devDependencies),
-                },
-            );
-        } else if path.is_dir() {
-            do_gen_relations(relations, &path);
+        // let path = dir.unwrap().path();
+        let dir = dir.unwrap();
+        let name = dir.file_name().to_str().unwrap().to_string();
+        if name.starts_with(".") {
+            if name != String::from(".pnpm") {
+                continue;
+            }
+        } else if name.starts_with("@") {
+            insert_relations_many(relations, &dir.path(), None);
+        } else {
+            insert_relations_one(relations, &dir.path(), None);
         }
+        // println!("{:?}", dir.unwrap().file_name());
     }
 }
 
 pub fn gen_relations() -> RelationsMap {
     let mut relations: RelationsMap = HashMap::default();
-    let base = fs::read_to_string("package.json").unwrap();
-    let Package {
-        name,
-        version,
-        dependencies,
-        devDependencies,
-    } = serde_json::from_str(&base).unwrap();
-    relations.insert(
-        "__root__".to_owned(),
-        Relation {
-            name,
-            version,
-            packages: merge_map(dependencies, devDependencies),
-        },
+    insert_relations_one(
+        &mut relations,
+        &PathBuf::from("."),
+        Some(String::from("__root__")),
     );
-    do_gen_relations(&mut relations, Path::new("node_modules"));
+    insert_relations(&mut relations, Path::new("node_modules"));
+    if Path::exists(&Path::new("pnpm-lock.yaml")) {
+        insert_relations(&mut relations, Path::new("node_modules/.pnpm/node_modules"));
+    }
     relations
 }
 
