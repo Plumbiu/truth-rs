86646eef57913c181d4a916f9faab0c5fa40ce3a
Plumbiu <3434909403@qq.com>
2023-12-08 19:55:12 +0800

    build: dev script

---
+++ scripts/dev.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @stylistic/template-tag-spacing */
+import { exec, execSync } from 'node:child_process'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import colors from 'picocolors'
+
+async function resolveDev() {
+  let retry = 0
+  const timer = setInterval(async () => {
+    try {
+      let start = Date.now()
+      const promises = [
+        fetch('http://localhost:8080/relations.json').then((res) => res.text()),
+        fetch('http://localhost:8080/tree.json').then((res) => res.text()),
+        fetch('http://localhost:8080/graph.json').then((res) => res.text()),
+      ]
+      const [relations, tree, graph] = await Promise.all(promises)
+      const writePath = path.join(process.cwd(), 'packages', 'web', 'public')
+      const writePromises = [
+        fs.writeFile(path.join(writePath, 'relations.json'), relations),
+        fs.writeFile(path.join(writePath, 'tree.json'), tree),
+        fs.writeFile(path.join(writePath, 'graph.json'), graph),
+      ]
+      await Promise.race(writePromises)
+      clearInterval(timer)
+      console.log(
+        '\n' +
+          colors.green(colors.bold('TRUTH-RS    ')) +
+          colors.dim('ready in ') +
+          colors.white(Date.now() - start) +
+          'ms\n',
+      )
+      console.log(
+        colors.bold('[SERVER]: ') +
+          colors.cyan(colors.underline('http://localhost:8080/api') + '\n'),
+      )
+      console.log(
+        colors.bold('[WEB]: ') +
+          colors.cyan(colors.underline('http://localhost:5174') + '\n'),
+      )
+      execSync('pnpm -F web run dev')
+      process.exit()
+    } catch (err) {
+      if (retry > 20) {
+        console.log(colors.red('Faild!'))
+        process.exit()
+      }
+      if (err.message === 'fetch failed') {
+        console.log(`server not start, waiting ~~~ x${++retry}`)
+      }
+    }
+  }, 1000)
+  exec('cargo run -- web')
+}
+
+resolveDev()
