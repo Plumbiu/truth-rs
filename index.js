d0a6aea6b2dd972e9bae0862e67a7bfc9f017212
Plumbiu <3434909403@qq.com>
2023-12-10 17:27:19 +0800

    fix: use __dirname

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 #! /usr/bin/env node
-import { argv, platform } from 'node:process'
-import { execFileSync } from 'node:child_process'
+const { argv, platform } = require('node:process')
+const { execFileSync } = require('node:child_process')
+const path = require('node:path')
 
-const params = argv.slice(2)
-
-if (platform === 'win32') {
-  execFileSync('bin/x86_64-pc-windows-gnu/truth-rs.exe', params, {
-    stdio: 'inherit',
-  })
-  console.log(cmd)
-} else if (platform === 'linux') {
-  execFileSync('bin/x86_64-unknown-linux-musl/truth-rs', params, {
-    stdio: 'inherit',
-  })
-} else if (platform === 'darwin') {
-  execFileSync('bin/x86_64-apple-darwin/truth-rs', params, {
-    stdio: 'inherit',
-  })
-} else {
-  console.log('unknown os')
+const binaryMap = {
+  win32: 'x86_64-pc-windows-gnu/truth-rs.exe',
+  linux: 'x86_64-unknown-linux-musl/truth-rs',
+  darwin: 'x86_64-apple-darwin/truth-rs',
 }
+
+const binPath = path.join(__dirname, 'bin')
+
+function execBinary() {
+  try {
+    let params = argv.slice(2)
+    execFileSync(path.join(binPath, binaryMap[platform]), params, {
+      stdio: 'inherit',
+    })
+  } catch (error) {}
+}
+
+execBinary()
